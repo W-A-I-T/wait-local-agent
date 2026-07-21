f56868946b330976eeb7267d2c2d11d6611432c5
nightcrawlerxme <43687337+nightcrawlerxme@users.noreply.github.com>
2026-07-20 17:19:55 -0700

    fix(connectors): export all cloud inventory connectors (#27)

--- src/wait_local_agent/cloud_connectors/__init__.py
+++ src/wait_local_agent/cloud_connectors/__init__.py
@@ -2,5 +2,12 @@ from __future__ import annotations
 
 from wait_local_agent.cloud_connectors.aws import AwsInventoryConnector
 from wait_local_agent.cloud_connectors.azure import AzureInventoryConnector
+from wait_local_agent.cloud_connectors.gcp import GCPInventoryConnector
+from wait_local_agent.cloud_connectors.m365 import M365InventoryConnector
 
-__all__ = ["AwsInventoryConnector", "AzureInventoryConnector"]
+__all__ = [
+    "AwsInventoryConnector",
+    "AzureInventoryConnector",
+    "GCPInventoryConnector",
+    "M365InventoryConnector",
+]
